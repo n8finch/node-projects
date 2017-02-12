074cbd5f954ee055667131c18f743ec5d226d213
Nate Finch <finchproservices@gmail.com>
2017-02-12 14:01:44 +0900

    node-todo packed

--- node-todo/app.js
+++ node-todo/app.js
@@ -7,7 +7,7 @@ var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public/app'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
